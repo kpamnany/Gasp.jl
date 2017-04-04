ca75335648168fac1859b505bf5b3db76a543104
kpamnany <kiran.pamnany@intel.com>
2017-04-04 09:53:29 -0700

    Enable OS-X. Support 0.6-dev.

--- deps/build.jl
+++ deps/build.jl
@@ -1,13 +1,13 @@
 target = "gasp/libgasp.$(Libdl.dlext)"
 
 if !isfile(target)
-    @static if is_linux()
+    @static if is_linux() || is_apple()
         repo = LibGit2.clone("https://github.com/kpamnany/gasp", "gasp")
         LibGit2.branch!(repo, "v0.4")
         println("Compiling libgasp...")
         run(`make -C gasp`)
     else
-	error("gasp is Linux-only right now")
+	error("gasp is currently Linux/OS-X only")
     end
 end
 
